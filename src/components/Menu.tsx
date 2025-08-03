3255aa97b9cb1f39297ced1be1b394c994527f15
Hajid Raihan <hajidraihan@gmail.com>
2025-08-03 18:17:21 +0700

    fixing bug

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
+import { Card, CardHeader, CardTitle } from "./ui/card";
 import { Badge } from "./ui/badge";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
 import { ImageWithFallback } from "./figma/ImageWithFallback";
